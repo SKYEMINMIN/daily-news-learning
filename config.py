3122ba46ce48ede8511e37f575217d3e182d3505
SKYEMINMIN <quwan00001@gmail.com>
2024-11-06 13:21:33 +0800

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class NewsProcessor:
         """从 NewsAPI 获取新闻"""
         try:
             url = "https://newsapi.org/v2/top-headlines"
-            api_key = "8156377f05e547f587b7bb470cece80e"
+            api_key = "70c47808e1fc40f2bb4450e822b5f2fc"
             
             params = {
                 "apiKey": api_key,
